b2bb509349e38e740395c00279196eeb4711e55a
Mohammad Jawad Hamdan <hamdanjawad789@gmail.com>
2026-01-06 16:11:58 +0200

    db config and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+dotenv.config();
 
 const app = express();
+
+// add auth object to the request
+app.use(clerkMiddleware()); 
+
 const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
+
+
+
